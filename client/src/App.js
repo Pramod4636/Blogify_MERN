429a54007e66527b0825339870d60e942ab92203
Pramod <pcdhaygude_b20@it.vjti.ac.in>
2023-06-03 00:04:10 +0530

    Initial Commit

---
+++ client/src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+
+import Login from "./component/Login"
+
+
+
+function App() {
+  return (
+    <div >
+      <Login />
+    </div>
+  );
+}
+
+export default App;
